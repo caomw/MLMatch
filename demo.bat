53839183286ec02d8f756128349a1dba8562436e
caomw <cmwqq2008@mail.hfut.edu.cn>
2020-07-06 16:02:56 +0800

    First commit.

---
+++ demo.bat
@@ -0,0 +1,12 @@
+MLMatch.exe ./input/colosseum/img1.jpg ./input/colosseum/img2.jpg
+rem MLMatch.exe ./input/theater/img2.jpg ./input/theater/img6.jpg
+rem MLMatch.exe ./input/uav/img1.jpg ./input/uav/img2.jpg
+rem MLMatch.exe ./input/building/img1.jpg ./input/building/img6.jpg
+rem MLMatch.exe ./input/dear/img1.jpg ./input/dear/img2.jpg
+rem MLMatch.exe ./input/rushmore/img1.jpg ./input/rushmore/img5.jpg
+rem MLMatch.exe ./input/graf/img1.ppm ./input/graf/img4.ppm
+rem MLMatch.exe ./input/bikes/img1.ppm ./input/bikes/img6.ppm
+rem LMatch.exe ./input/wall/img1.ppm ./input/wall/img6.ppm
+
+
+pause
